a49dadcb5b0e398b12d9b853b743b0cdc355cb0d
Geun-Oh <kandy1002@naver.com>
2022-04-06 14:20:10 +0000

    basic-auth

---
+++ basic-auth/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "basicauthb8b8a2df": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
