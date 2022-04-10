ef8d7be767de23dc4c88c49e4b609f7c123efa2a
Geun-Oh <kandy1002@naver.com>
2022-04-10 01:00:46 +0000

    image modifying Trigger added

--- lambda-trigger-example/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ lambda-trigger-example/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,9 +1,32 @@
 export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "lambdatriggerexample2e58ad49": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
     "function": {
         "lambdatriggerexample2e58ad49PostConfirmation": {
             "Name": "string",
             "Arn": "string",
             "LambdaExecutionRole": "string",
+            "Region": "string"
+        },
+        "S3Trigger8bfbf280": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "imagemodify": {
+            "BucketName": "string",
             "Region": "string"
         }
     }
--- lambda-trigger-example/src/App.js
+++ lambda-trigger-example/src/App.js
@@ -1,35 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import { Auth } from 'aws-amplify';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import React, { useState, useEffect } from 'react';
+import { Storage } from 'aws-amplify';
+import { v4 as uuid } from 'uuid';
 import './App.css';
 
 function App() {
-  const [user, updateUser] = useState(null)
+  const [images, setImages] = useState([])
   useEffect(() => {
-    Auth.currentAuthenticatedUser()
-      .then(user => updateUser(user))
-      .catch(err => console.log(err))
+    fetchImages()
   }, [])
-  let isAdmin = false
-  if (user) {
-    const { signInUserSession: {idToken: { payload }} } = user
-    console.log('payload: ', payload)
-    if (
-      payload['cognito:groups'] &&
-    payload['cognito:groups'].includes('Admin')
-    ) {
-      isAdmin = true
-    }
+  async function onChange(e) {
+    // 파일이 업로드되면 고유한 이름을 생성하고 Storage API를 사용해 저장한다.
+    const file = e.target.files[0]
+    const filetype = file.name.split('.')[file.name.split.length - 1]
+    await Storage.put(`${uuid()}.${filetype}`, file)
+    // 파일이 업로드되면 이미지 리스트를 가져온다.
+    fetchImages()
   }
+  async function fetchImages() {
+    // S3 버킷에서 이미지 키 리스트를 가져온다.
+    const files = await Storage.list('')
+    // 키가 있으면 이미지가 보여지도록 인증(signed)되어야 한다.
+    const signedFiles = await Promise.all(files.map(async file => {
+      /**
+       * 이미지가 인증되려면 이미지 키 배열을 통해 각 이미지에 대한 인증된 URL(signed URL)을 가져와야 한다.
+       */
+      const signedFile = await Storage.get(file.key)
+      return signedFile
+    }))
+    setImages(signedFiles)
+  }
+
   return (
     <div className='App'>
-      <header>
-        <h1>Hello World</h1>
-        { isAdmin && <p>Welcome, Admin</p> }
+      <header className='App-header'>
+        <input
+          type='file'
+          onChange={onChange}
+        />
+        {
+          images.map(image => (
+            <img
+              alt={image}
+              src={image}
+              key={image}
+              style={{ width: 500 }}
+            />
+          ))
+        }
       </header>
-      <AmplifySignOut />
     </div>
   )
 }
 
-export default withAuthenticator(App);
+export default App;
